911298ebe0c028c6e23e51d75d22fbd15c59415d
aleksandra <aleksandra.ovsia@gmail.com>
2026-08-27 14:54:48 +0300

    eds

--- wp-content/themes/instintut/functions.php
+++ wp-content/themes/instintut/functions.php
@@ -1042,22 +1042,3 @@ function html5blankcomments($comment, $args, $depth)
                     }
                 ));
             });
-
-            //Отображение текущего шаблона
-            add_filter('template_include', 'var_template_include', 1000);
-            function var_template_include($t)
-            {
-                $GLOBALS['current_theme_template'] = basename($t);
-                return $t;
-            }
-
-            function get_current_template($echo = false)
-            {
-
-                if (!isset($GLOBALS['current_theme_template']))
-                    return false;
-                if ($echo)
-                    echo $GLOBALS['current_theme_template'];
-                else
-                    return $GLOBALS['current_theme_template'];
-            }
